6eb0a70ff4c8311ca090d8020cd4295c45a679f5
MuriloFlores <muriloaflores@gmail.com>
2024-04-18 20:30:34 -0300

    inital test

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
